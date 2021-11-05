544d89292cc1a281febe9b9e58eca780b25d12c5
Kevin Staight <kevin_staight@hotmail.com>
2021-11-05 05:53:27 +0000

    groovy

--- jenkins_dsl.groovy
+++ jenkins_dsl.groovy
@@ -8,11 +8,4 @@ job('python example') {
     triggers {
         scm('H/5 * * * *')
     }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
-    steps {
-        shell("npm install")
-    }
 }
